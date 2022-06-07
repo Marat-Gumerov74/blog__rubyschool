6b4a4ed49c52aa7b7a8e147be8b268b4b04b5ba5
Marat Gumerov <gumerovm74@gmail.com>
2022-06-07 23:42:52 +0300

    articles/ edit  & update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,14 +2,31 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+  
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
-    if @article.valid?
-      @article.save
+    if @article.save
       redirect_to @article
     else
       render action: 'new'
